0a56c9e6d3ef8a949a32de5d829cb7d828851ff2
gurkebaui <136100156+gurkebaui@users.noreply.github.com>
2025-04-09 18:32:30 +0200

    Koenigsegg, animation
    added koenigsegg with animation

--- Test Gayme/core/src/main/java/io/github/some_example_name/Main.java
+++ Test Gayme/core/src/main/java/io/github/some_example_name/Main.java
@@ -1,34 +1,120 @@
 package io.github.some_example_name;
 
+
+
+
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.math.Vector3;
+import net.mgsx.gltf.loaders.gltf.GLTFLoader;
+import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute;
+import net.mgsx.gltf.scene3d.attributes.PBRTextureAttribute;
+import net.mgsx.gltf.scene3d.lights.DirectionalLightEx;
+import net.mgsx.gltf.scene3d.scene.Scene;
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
+import net.mgsx.gltf.scene3d.scene.SceneManager;
+import net.mgsx.gltf.scene3d.scene.SceneSkybox;
+import net.mgsx.gltf.scene3d.utils.IBLBuilder;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends ApplicationAdapter
+{
+    private SceneManager sceneManager;
+    private SceneAsset sceneAsset;
+    private Scene scene;
+    private PerspectiveCamera camera;
+    private Cubemap diffuseCubemap;
+    private Cubemap environmentCubemap;
+    private Cubemap specularCubemap;
+    private Texture brdfLUT;
+    private float time;
+    private SceneSkybox skybox;
+    private DirectionalLightEx light;
+    private FirstPersonCameraController cameraController;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+
+        // create scene
+        sceneAsset = new GLTFLoader().load(Gdx.files.internal("Models/drive.gltf"));
+        scene = new Scene(sceneAsset.scene);
+        sceneManager = new SceneManager();
+        sceneManager.addScene(scene);
+
+        // setup camera (The BoomBox model is very small so you may need to adapt camera settings for your scene)
+        camera = new PerspectiveCamera(60f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        float d = .02f;
+        camera.near = d / 1000f;
+        camera.far = 200;
+        sceneManager.setCamera(camera);
+        camera.position.set(0,0.5f, 4f);
+
+
+
+        cameraController = new FirstPersonCameraController(camera);
+        Gdx.input.setInputProcessor(cameraController);
+
+        // setup light
+        light = new DirectionalLightEx();
+        light.direction.set(1, -3, 1).nor();
+        light.color.set(Color.WHITE);
+        sceneManager.environment.add(light);
+
+        // setup quick IBL (image based lighting)
+        IBLBuilder iblBuilder = IBLBuilder.createOutdoor(light);
+        environmentCubemap = iblBuilder.buildEnvMap(1024);
+        diffuseCubemap = iblBuilder.buildIrradianceMap(256);
+        specularCubemap = iblBuilder.buildRadianceMap(10);
+        iblBuilder.dispose();
+
+        // This texture is provided by the library, no need to have it in your assets.
+        brdfLUT = new Texture(Gdx.files.classpath("net/mgsx/gltf/shaders/brdfLUT.png"));
+
+        sceneManager.setAmbientLight(1f);
+        sceneManager.environment.set(new PBRTextureAttribute(PBRTextureAttribute.BRDFLUTTexture, brdfLUT));
+        sceneManager.environment.set(PBRCubemapAttribute.createSpecularEnv(specularCubemap));
+        sceneManager.environment.set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap));
+
+        // setup skybox
+        skybox = new SceneSkybox(environmentCubemap);
+        sceneManager.setSkyBox(skybox);
+
+        scene.animationController.setAnimation("driving",-1);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        sceneManager.updateViewport(width, height);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        time += deltaTime;
+
+        cameraController.update();
+        if(Gdx.input.isButtonJustPressed(Input.Keys.SPACE))
+            scene.animationController.setAnimation("left");
+
+
+        // render
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        sceneManager.update(deltaTime);
+        sceneManager.render();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        sceneManager.dispose();
+        sceneAsset.dispose();
+        environmentCubemap.dispose();
+        diffuseCubemap.dispose();
+        specularCubemap.dispose();
+        brdfLUT.dispose();
+        skybox.dispose();
     }
 }
